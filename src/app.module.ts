d606ccc99da34fc2b8eb3c074bea37bb651c6783
Sajad Rastegar <rastegar.sajjad@gmail.com>
2023-07-23 22:43:17 +0200

    Rename geo_jsons folder to features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FeaturesModule } from './geo_jsons/features.module';
+import { FeaturesModule } from './features/features.module';
 
 @Module({
   imports: [FeaturesModule],
